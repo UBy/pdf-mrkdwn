3f62a6a51b79d7ec5cb6a13ed5426daa245d2545
Ulf Byskov <ulf.byskov@gmail.com>
2019-10-01 15:04:25 +0300

    Unsolved git conflict

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-<<<<<<< HEAD
     "name": "README",
     "output": ".",
     "wkhtmltopdf": {
@@ -11,20 +10,5 @@ module.exports = {
         "footerFontSize": "8",
         "footerSpacing": "10",
         "marginBottom": "24"
-=======
-    'name':   'MyBook',
-    'input':  '.',
-    'output': '.',
-    'css':    `${__dirname}/style.css`,
-    'wkhtmltopdf': {
-
-        'zoom': '1.5',
-        'pageWidth': '5.5in',
-        'pageHeight': '8.5in',
-        'footerCenter': '[page]',
-        'footerFontSize': '8',
-        'footerSpacing': '10',
-        'marginBottom': '24'
->>>>>>> 4344c4c5d4b3334a8b435e3b974c6adebdd4a26d
     }
 }
